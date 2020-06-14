aa04d7778cfe14398ae0891610815b7216611c58
hepuyin <1548513100@qq.com>
2020-06-14 20:09:41 +0800

    第一次提交

---
+++ src/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * IDEA上的Git的使用
+ * @author null_he
+ * @create 2020-06-14 20:08
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hellt Git!");
+    }
+}
